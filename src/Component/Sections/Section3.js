50d4672780ecd16916a17c53c4940a7ddfd7625d
nowshin2 <nowshin@algosolver.com>
2022-04-15 19:02:31 +0600

    Create Section3.js

---
+++ src/Component/Sections/Section3.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import '../Styles/style_section.scss';
+
+function Section3() {
+  return (
+    <div>
+        <div className='background-color3 text-center alignItem'>
+             <div class="text-4xl font-bold pt-12">Top Categories</div>
+
+             <div class="flex justify-center gap-10 pt-12 ">
+                <div class=" h-20 w-60 shadow-md bg-white flex  gap-5">
+                    <div class="bg-white pt-7 pl-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[#FF6752]" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                        </svg>    
+                    </div> 
+                    <div>
+                        <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-6">Design Dave</h5>
+                    </div>
+                </div>
+
+                <div class=" h-20 w-60 shadow-md bg-white flex gap-5">
+                    <div class="bg-white pt-7 pl-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[#FF6752]" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+                        </svg> 
+                    </div> 
+                    <div>
+                        <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-7">Development</h5>
+                    </div>
+                </div>
+
+                <div class=" h-20 w-60 shadow-md bg-white">
+                    <div class=" h-20 w-60 shadow-md bg-white flex gap-5">
+                        <div class="bg-white pt-7 pl-5">
+                            <svg class="h-8 w-8 text-[#FF6752]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clip-rule="evenodd"></path></svg>
+                        </div> 
+                        <div>
+                            <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-7">Marketing</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class=" h-20 w-60 shadow-md bg-white">
+                    <div class=" h-20 w-60 shadow-md bg-white flex gap-5">
+                        <div class="bg-white pt-7 pl-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[#FF6752]" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M2 5a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2V5zm14 1a1 1 0 11-2 0 1 1 0 012 0zM2 13a2 2 0 012-2h12a2 2 0 012 2v2a2 2 0 01-2 2H4a2 2 0 01-2-2v-2zm14 1a1 1 0 11-2 0 1 1 0 012 0z" clip-rule="evenodd" />
+                        </svg>
+                        </div> 
+                        <div>
+                            <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-7">It & Software</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class=" flex justify-center gap-10 pt-10 ">
+                <div class=" h-20 w-60 shadow-md bg-white">
+                    <div class=" h-20 w-60 shadow-md bg-white flex gap-5">
+                        <div class="bg-white pt-7 pl-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[#FF6752]" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                            </svg>
+                        </div> 
+                        <div>
+                            <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-7">Videography</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class=" h-20 w-60 shadow-md bg-white">
+                    <div class=" h-20 w-60 shadow-md bg-white flex gap-5">
+                        <div class="bg-white pt-7 pl-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[#FF6752]" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd" />
+                            <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+                        </svg>
+                        </div> 
+                        <div>
+                            <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-7">Business Sys</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class=" h-20 w-60 shadow-md bg-white">
+                    <div class=" h-20 w-60 shadow-md bg-white flex gap-5">
+                        <div class="bg-white pt-7 pl-5">
+                        <svg class="h-8 w-8 text-[#FF6752]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
+                        </div> 
+                        <div>
+                            <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-7">Photography</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class=" h-20 w-60 shadow-md bg-white">
+                    <div class=" h-20 w-60 shadow-md bg-white flex gap-5">
+                        <div class="bg-white pt-7 pl-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-[#FF6752]" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm9 4a1 1 0 10-2 0v6a1 1 0 102 0V7zm-3 2a1 1 0 10-2 0v4a1 1 0 102 0V9zm-3 3a1 1 0 10-2 0v1a1 1 0 102 0v-1z" clip-rule="evenodd" />
+                        </svg>
+                        </div> 
+                        <div>
+                            <h5 class="mb-2 text-[22px] font-semibold tracking-tight text-gray-900 dark:text-white pt-7">Design Dave</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</div>
+  )
+}
+
+export default Section3
