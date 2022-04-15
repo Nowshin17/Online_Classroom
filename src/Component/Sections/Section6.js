d2c9dbc39af5be38acfcd28b62f20ffb997ec25d
nowshin2 <nowshin@algosolver.com>
2022-04-15 19:02:52 +0600

    Create Section6.js

---
+++ src/Component/Sections/Section6.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+
+function Section6() {
+  return (
+    <div>
+         <div className='background-color6 alignItem flex justify-center gap-40'>
+       
+            <div className='text-center pt-20'>
+                <div class="text-3xl font-bold pt-1"> 
+                    <button class="p-4 border rounded-full bg-[#255D52] border-transparent text-neutral-50">
+                    <svg class="h-8 w-8 "  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+                    </svg>
+                    </button>
+                </div>
+                <div class="text-3xl font-bold pt-2"> 276K</div>
+                <div class="pl-12 pt-2"> <div class="border-solid border-2 border-[#FF6752] w-8 "></div></div>
+                <div class=" pt-2 text-gray-600"> Worldwide Student</div>
+            </div>
+
+            <div className='text-center pt-20'>
+                <div class="text-3xl font-bold pt-1"> 
+                    <button class="p-4 border rounded-full bg-[#255D52] border-transparent text-neutral-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
+                    </button>
+                </div>
+                <div class="text-3xl font-bold pt-2">23+</div>
+                <div class="pl-12 pt-2"> <div class="border-solid border-2 border-[#FF6752] w-8 "></div></div>
+                <div class=" pt-2 text-gray-600"> Years Experience</div>
+            </div>
+
+            <div className='text-center pt-20'>
+                <div class="text-3xl font-bold pt-1"> 
+                    <button class="p-4 border rounded-full bg-[#255D52] border-transparent text-neutral-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+                    </svg>
+                    </button>
+                </div>
+                <div class="text-3xl font-bold pt-2"> 735K+</div>
+                <div class="pl-12 pt-2"> <div class="border-solid border-2 border-[#FF6752] w-8 "></div></div>
+                
+                <div class=" pt-2 text-gray-600"> Professional Course</div>
+            </div>
+
+            <div className='text-center pt-20'>
+                <div class="text-3xl font-bold pt-1"> 
+                    <button class="p-4 border rounded-full bg-[#255D52] border-transparent text-neutral-50">
+                        <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path></svg>
+                    </button>
+                </div>
+                <div class="text-3xl font-bold pt-2"> 4.3K+</div>
+                <div class="pl-12 pt-2"> <div class="border-solid border-2 border-[#FF6752] w-8 "></div></div>
+                <div class=" pt-2 text-gray-600">Beautiful Review</div>
+            </div>
+
+         </div>
+    </div>
+  )
+}
+
+export default Section6
