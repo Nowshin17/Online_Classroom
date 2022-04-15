5cd207fa06a517c98074395c740a7362457d5fcc
nowshin2 <nowshin@algosolver.com>
2022-04-15 19:02:48 +0600

    Create Section5.js

---
+++ src/Component/Sections/Section5.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function Section5() {
+  return (
+    <div>
+        <div className='background-color4 alignItem flex'>
+
+            <div className='background-color2'  class="pt-40">
+                <img src="./img2.png" alt="img" width="600px" height="600px"  /> 
+            </div>
+
+            <div class="pt-40 pl-40">
+                <div> <img src="./icon.png" alt="img" width="120px" height="120px" />  </div>
+                <div class="text-[#ff6752] text-1xl font-semibold pt-10">Why Choose Us</div>
+                <div class="text-4xl font-bold pt-1"> Why You Choose our <br/>Online Learing</div>
+                <div class=" pt-6 text-gray-600"> Dramatically supply tranparent deliverables before</div>
+                <div class="text-gray-600">backward internal or "organic" sources.</div>
+
+                <div class="flex gap-3 pt-6">
+                    <div class="text-[#255D52]">
+                        <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <div class="font-semibold">
+                    Increasing Your Learing Skills
+                    </div>
+                </div>
+
+                <div class="flex gap-3 pt-2">
+                    <div class="text-[#255D52]">
+                        <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <div class="font-semibold">
+                    Hight Quality Video & Audio Classes
+                    </div>
+                </div>
+
+                <div class="flex gap-3 pt-2">
+                    <div class="text-[#255D52]">
+                        <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <div class="font-semibold">
+                    Finish You Course, Get Certificate
+                    </div>
+                </div>
+
+
+                <div className='logo pt-10 '>
+                    <button class="bg-[#ff6752] text-white font-medium py-2 px-4 border border-[#ff6752] rounded">
+                        More Details
+                    </button>
+                </div>
+
+            </div>
+
+        </div>         
+    </div>
+  )
+}
+
+export default Section5
