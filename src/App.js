2d6eeda7241141e6acca0e3d96dcd8f6c3ad76a4
nowshin2 <nowshin@algosolver.com>
2022-04-15 19:01:34 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Component/Footer/Footer';
+import Navbar from './Component/Navbar/Navbar';
+import Section1 from './Component/Sections/Section1';
+import Section10 from './Component/Sections/Section10';
+import Section11 from './Component/Sections/Section11';
+import Section2 from './Component/Sections/Section2';
+import Section3 from './Component/Sections/Section3';
+import Section4 from './Component/Sections/Section4';
+import Section5 from './Component/Sections/Section5';
+import Section6 from './Component/Sections/Section6';
+import Section7 from './Component/Sections/Section7';
+import Section8 from './Component/Sections/Section8';
+import Section9 from './Component/Sections/Section9';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    
+     <Navbar />
+     <Section1 />
+     <Section2 />
+     <Section3 />
+     <Section4 />
+     <Section5 />
+     <Section6 />
+     <Section7 />
+     <Section8 />
+     <Section9 />
+     <Section10 />
+     <Section11 />
+     <Footer />
+
+     
+    
+      
+ </div>
   );
 }
 
