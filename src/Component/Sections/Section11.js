5a779051b8eccf986ed8a30249f98b1841963fb2
nowshin2 <nowshin@algosolver.com>
2022-04-15 19:02:20 +0600

    Create Section11.js

---
+++ src/Component/Sections/Section11.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../Styles/style_section.scss';
+
+function Section11() {
+  return (
+    <div>
+        <div className='background-color11 alignItem '>
+        <div className='text-center'>
+            <div class="text-4xl font-bold pt-1">Our Key Supports</div> 
+            <div class="flex gap-40 pt-16">
+                <div class="box-border h-32 w-32 p-4 shadow-2xl"> 
+                    <div class="pt-4 pl-1">
+                    <img src="./1t.png" alt="img" /> 
+                    
+                    </div>
+                </div>
+
+                <div class="box-border h-32 w-32 p-4 shadow-2xl"> 
+                    <div class="pt-8 pl-6">
+                        <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clip-rule="evenodd"></path></svg>
+                    </div>
+                </div>
+
+                <div class="box-border h-32 w-32 p-4 shadow-2xl"> 
+                    <div class="pt-8 pl-6">
+                        <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clip-rule="evenodd"></path></svg>
+                    </div>
+                </div>
+
+                <div class="box-border h-32 w-32 p-4 shadow-2xl"> 
+                    <div class="pt-8 pl-6">
+                        <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clip-rule="evenodd"></path></svg>
+                    </div>
+                </div>
+
+                <div class="box-border h-32 w-32 p-4 shadow-2xl"> 
+                    <div class="pt-8 pl-6">
+                        <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clip-rule="evenodd"></path></svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Section11
